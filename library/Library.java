a8d86c15285e0a5bd12a38ddb64e235d28caf837
Loris Bach <lolobach01100@gmail.com>
2026-02-24 12:06:03 +0100

    Add Main, Book, and Library classes with basic functionality

---
+++ library/Library.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public Book searchBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
